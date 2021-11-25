c8e27e2994ee15fc9637368526b07b471d96cf03
Lugovskoi <58656757+Lugovskoi@users.noreply.github.com>
2021-11-26 01:41:21 +0300

    Pogodabot ready

--- labs/lab01/pogodabot.py
+++ labs/lab01/pogodabot.py
@@ -1,9 +1,22 @@
+import pyowm
 import telebot
+from pyowm.owm import OWM
+from pyowm.utils.config import get_default_config
+config_dict = get_default_config()
+config_dict['language'] = 'ru'  
 
+owm = pyowm.OWM('2158c8392bfa80b602e4110ac47ce933')
 bot = telebot.TeleBot("2115511291:AAFqBR2y-h1_BgUVIBbuDt_Xg6e8FYOaWSw")
 
 @bot.message_handler(content_types=['text'])
 def send_mes(message):
-	bot.send_message(message.chat.id, message.text)
+    mrg = owm.weather_manager()
+    place = message.text
+    observation = mrg.weather_at_place(place)
+    w = observation.weather
+    temp = w.temperature('celsius')['temp']
+    answer = "В городе " + place + ": " + w.detailed_status + "\n"
+    answer += "Температура: " + str(temp) + "\n\n"
+    bot.send_message(message.chat.id, answer)
 
 bot.polling(none_stop = True)
